561d82b0565532efd4124be5437f194122ac2256
fredy-me <fredymelkizedek@gmail.com>
2026-06-24 22:46:49 +0300

    create inital data type

--- DataType.java
+++ DataType.java
@@ -1,7 +1,12 @@
 public class DataType {
 
     public static void main(String[] args) {
+        
         // Declare and initialize variables of different data types
+        int age = 25;
+        double height = 5.9;
+        char grade = 'A';
+        boolean isStudent = true;
     }
     
 }
